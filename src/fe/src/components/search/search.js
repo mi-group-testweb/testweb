63bd9a6d36bae84d588c4039a500848a121ac640
malinli <malinli@xiaomi.com>
2019-08-22 19:44:32 +0800

    search edit

--- src/fe/src/components/search/search.js
+++ src/fe/src/components/search/search.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import './search.css';
 import { SearchBar } from 'antd-mobile';
-  
+import {ButtonGroup,Button,DropdownButton} from 'react-bootstrap/ButtonGroup'
+var Dropdown = require("react-bootstrap-dropdown");
 class SearchBarExample extends React.Component {
     state = {
-        
+
     };
     componentDidMount() {
     }
     onChange = (value) => {
         this.setState({ value });
     };
+
     render() {
         return (<div>
+            <ButtonGroup vertical>
+                <Button>Button</Button>
+                <Button>Button</Button>
+                <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-vertical-dropdown-1">
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+                <Button>Button</Button>
+                <Button>Button</Button>
+                <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-vertical-dropdown-2">
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+                <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-vertical-dropdown-3">
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                </DropdownButton>
+            </ButtonGroup>
             <SearchBar
                 value={this.state.value}
                 placeholder="Search"
