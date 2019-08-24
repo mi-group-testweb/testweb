dac0e33a8aa235690479c5c95101f32aee4c2192
chengshihao <chengsh1018@163.com>
2019-08-24 15:04:47 +0800

    two simple interface to get and send data

--- src/ge/Server/main.go
+++ src/ge/Server/main.go
@@ -26,14 +26,112 @@ type Task struct {
 	Bullet     json.RawMessage `json:"bullet"`
 }
 
-type PINGBullet struct {
-	Host      string `json:"host"`
-	Count     int    `json:"count"`
-	DataBytes int    `json:"data_bytes"`
-	Interval  int    `json:"interval"`
-	Debug     struct {
-		Mtr string `json:"mtr"`
+type HTTPBullet struct {
+	Url         string                 `json:"url"`
+	Method      string                 `json:"method"`
+	Header      map[string]interface{} `json:"header"`
+	ContentType string                 `json:"content_type"`
+	Body        string                 `json:"body"`
+	MatchResult struct {
+		MatchHeader  bool   `json:"match_header"`
+		MatchBody    bool   `json:"match_body"`
+		MatchContent string `json:"match_content"`
+	} `json:"match_result"`
+	ReturnHeader bool `json:"return_header"`
+	Redirect     bool `json:"redirect"`
+	Compress     bool `json:"compress"`
+	Timeout      int  `json:"timeout"`
+	Debug        struct {
+		Ping string `json:"ping"`
+		Mtr  string `json:"mtr"`
+		Dig  string `json:"dig"`
 	} `json:"debug"`
+	Resolve struct {
+		Enabled bool   `json:"enabled"`
+		Host    string `json:"host"`
+		Port    int    `json:"port"`
+		Address string `json:"address"`
+	} `json:"resolve"`
+	Ldns struct {
+		Enabled bool   `json:"enabled"`
+		Server  string `json:"server"`
+	} `json:"ldns"`
+}
+
+type StackData struct {
+	TaskId
+	Err
+	Type  string          `json:"type"`
+	Raw   string          `json:"raw"`
+	Track json.RawMessage `json:"track"`
+}
+
+type Err struct {
+	Message   string `json:"msg"`
+	Code      int    `json:"code"`
+	ErrorType string `json:"error_type"`
+	Error     bool   `json:"error"`
+	ParseErr  string `json:"-"`
+}
+
+type DataArr struct {
+	Data   []StackData    `json:"data"`
+}
+
+type HTTPTrack struct {
+	HTTPTime
+	HTTPTimeSlice
+	ContentType  string  `json:"content_type"`
+	Speed        float64 `json:"speed"`
+	ConnectNum   float64 `json:"num_connects"`
+	RedirectUrl  string  `json:"redirect_url"`
+	RedirectNum  float64 `json:"num_redirects"`
+	DownloadSize float64 `json:"size_download"`
+	ServerIP     string  `json:"server_ip"`
+	// UrlEffective string  `json:"url_effective"`
+	Ldns         string `json:"ldns"`
+	IsMatch      bool   `json:"is_match"`
+	Header       string `json:"header"`
+	ResponseCode int    `json:"rc"`
+	HTTPDebug
+	Load string `json:"load"`
+	At
+}
+
+type HTTPTime struct {
+	DNSResolveTime  float64 `json:"time_namelookup"`
+	ConnectionTime  float64 `json:"time_connect"`
+	RedirectTime    float64 `json:"time_redirect"`
+	AppConnectTime  float64 `json:"time_appconnect"`
+	PretransferTime float64 `json:"time_pretransfer"`
+	FirstByteTime   float64 `json:"time_starttransfer"`
+	ResponseTime    float64 `json:"time_total"`
+}
+
+type HTTPTimeSlice struct {
+	Dnsrt float64 `json:"dnsrt"`
+	Ct    float64 `json:"ct"`
+	Rdt   float64 `json:"rdt"`
+	Act   float64 `json:"act"`
+	Fbt   float64 `json:"fbt"`
+	Cdt   float64 `json:"cdt"`
+	Rbt   float64 `json:"rbt"`
+	Rt    float64 `json:"rt"`
+}
+
+type HTTPDebug struct {
+	Url   string `json:"url"`
+	Debug struct {
+		Ping string `json:"ping"`
+		Dig  string `json:"dig"`
+		Mtr  string `json:"mtr"`
+	} `json:"debug"`
+}
+
+type At struct {
+	AssignedAt  string `json:"assigned_at"`
+	RunAt       string `json:"run_at"`
+	CollectedAt string `json:"collected_at"`
 }
 
 type TestURL struct {
@@ -43,7 +141,7 @@ type TestURL struct {
 func main() {
 	http.HandleFunc("/url", GetUrl)
 	http.HandleFunc("/v2/assignments", SendMission)
-	//http.HandleFunc("/v2/assignments", Send_Mission)
+	http.HandleFunc("/v2/tracks", GetResult)
 	fmt.Println("Listening...")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8000", nil))
 }
@@ -79,11 +177,13 @@ func SendMission(w http.ResponseWriter, r *http.Request) {
 			MissionIntanceId: "",
 			MissileId:        0,
 		},
-		Type: "PING",
+		Type: "HTTP",
 	}
-	var bullet PINGBullet
-	bullet.Host = TestIP
-	bullet.Count = 4
+	var bullet HTTPBullet
+	bullet.Url = TestIP
+	bullet.Method = "POST"
+	bullet.Redirect = false
+	bullet.Timeout = 60
 	byte1, err := json.Marshal(bullet)
 	if err != nil {
 		panic(err)
@@ -97,3 +197,19 @@ func SendMission(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func GetResult(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	var stack DataArr
+	err = json.Unmarshal(body, &stack)
+	if err != nil {
+		panic(err)
+	}
+	var HTTPResult HTTPTrack
+	err = json.Unmarshal(stack.Data[0].Track, &HTTPResult)
+	_, _ = fmt.Fprintf(w, " DNSTime : %+v\n", HTTPResult.DNSResolveTime)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
